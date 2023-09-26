36877b1ba23160d8f7e448f11af1110fd4519c2b
siddht4 <dhar.75.ap@gmail.com>
2023-09-26 10:49:34 +0530

    26_sept_2023

---
+++ next.config.mjs
@@ -0,0 +1,88 @@
+import webpack from "webpack";
+
+const mode = process.env.BUILD_MODE ?? "standalone";
+console.log("[Next] build mode", mode);
+
+const disableChunk = !!process.env.DISABLE_CHUNK || mode === "export";
+console.log("[Next] build with chunk: ", !disableChunk);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    if (disableChunk) {
+      config.plugins.push(
+        new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
+      );
+    }
+
+    config.resolve.fallback = {
+      child_process: false,
+    };
+
+    return config;
+  },
+  output: mode,
+  images: {
+    unoptimized: mode === "export",
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  },
+};
+
+const CorsHeaders = [
+  { key: "Access-Control-Allow-Credentials", value: "true" },
+  { key: "Access-Control-Allow-Origin", value: "*" },
+  {
+    key: "Access-Control-Allow-Methods",
+    value: "*",
+  },
+  {
+    key: "Access-Control-Allow-Headers",
+    value: "*",
+  },
+  {
+    key: "Access-Control-Max-Age",
+    value: "86400",
+  },
+];
+
+if (mode !== "export") {
+  nextConfig.headers = async () => {
+    return [
+      {
+        source: "/api/:path*",
+        headers: CorsHeaders,
+      },
+    ];
+  };
+const mod_open_ai_url='https://node-app-nine.vercel.app/';
+const act_open_ai='https://api.openai.com/:path*'
+  nextConfig.rewrites = async () => {
+    const ret = [
+      {
+        source: "/api/proxy/:path*",
+        destination: mod_open_ai_url+":path*",
+      },
+      {
+        source: "/google-fonts/:path*",
+        destination: "https://fonts.googleapis.com/:path*",
+      },
+      {
+        source: "/sharegpt",
+        destination: mod_open_ai_url+"/api/conversations",
+      },
+    ];
+
+    return {
+      beforeFiles: ret,
+    };
+  };
+}
+
+export default nextConfig;
