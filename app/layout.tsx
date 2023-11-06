c3400178ace627d9ef046b0d73c4f283fa33bac7
siddht1 <rt1yrru+github@gmail.com>
2023-11-06 09:38:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Cat-lon-3b",
+  title: "Cat-lon-3c",
   description: "ChatBot by KIVTECHS",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "CATLON-3C",
     statusBarStyle: "default",
   },
 };
